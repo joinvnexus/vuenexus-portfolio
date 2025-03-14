c267b7a581c39a2fd549e3eed6b286d575fb053c
projoy-Vue <projoynaidu407@gmail.com>
2025-03-14 23:21:53 +0600

    Added hello.txt

--- src/components/AboutMe.vue
+++ src/components/AboutMe.vue
@@ -59,7 +59,7 @@
 </template>
 
 <script>
-import imgurl from '../assets/photos/Responsive WordPress E-commerce Design.jpg'
+import imgurl from '../assets/photos/Responsive WordPress E-commerce Design1.jpg'
 export default {
   name: "AboutMe",
   data() {
